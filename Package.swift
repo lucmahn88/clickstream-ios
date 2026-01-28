3b97ae94321862a8305a7bff473144eecec1733d
Luqman <luqman.fauzi@gojek.com>
2026-01-28 15:03:10 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Clickstream",
     platforms: [.macOS(.v10_12),
-                .iOS(.v11),
+                .iOS(.v14),
                 .tvOS(.v10),
                 .watchOS(.v3)],
     products: [
@@ -16,17 +16,19 @@ let package = Package(
             targets: ["Clickstream"]),
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .exact("1.10.2")),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.30.0"),
         .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
-        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift.git", .exact("5.12.0")),
-        .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.4")),
+        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift.git", from: "6.7.0"),
+        .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.5")),
+        .package(name: "CourierCore", url: "https://github.com/gojek/courier-iOS.git", from: "1.0.0"),
+        .package(name: "CourierMQTT", url: "https://github.com/gojek/courier-iOS.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Clickstream",
-            dependencies: ["SwiftProtobuf", "Reachability", "GRDB", "Starscream"],
+            dependencies: ["SwiftProtobuf", "Reachability", "GRDB", "Starscream", "CourierCore", "CourierMQTT"],
             path: "Sources"),
         .testTarget(
             name: "ClickstreamTests",
